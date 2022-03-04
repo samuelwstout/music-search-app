8a47e0003826eb6ca8a58c877f434cc8b0fd9d92
samuelwstout <samuelwstout@gmail.com>
2022-03-04 03:07:27 -0600

    request authorization without currently  unnecessary functions

--- index.js
+++ index.js
@@ -31,59 +31,6 @@ const redirectURI = 'http://127.0.0.1:5500/index.html'
 const scope = 'user-read-private user-read-email'
 const TOKEN = 'https://accounts.spotify.com/api/token'
 
-const onPageLoad = () => {
-    if (window.location.search.length > 0) {
-        handleRedirect()
-    }
-}
-
-const handleRedirect = () => {
-    let code = getCode()
-    fetchAccessToken( code )
-}
-
-const fetchAccessToken = ( code ) => {
-    let body = 'grant_type=authorization_code'
-    body += '&code=' + code
-    body += '&redirect_uri=' + encodeURI(redirectURI)
-    body += '&client_id=' + clientID
-    body += '&client_secret=' + clientSecret
-    callAuthorizationApi(body)
-}
-
-const callAuthorizationApi = (body) => {
-    let xhr = new XMLHttpRequest()
-    xhr.open('POST', TOKEN, true)
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-    xhr.setRequestHeader('Authorization', 'Basic ' + Buffer.from(clientID + ':' + clientSecret).toString('base64'))
-    xhr.send(body)
-    xhr.onload = handleAuthorizationResponse
-}
-
-const handleAuthorizationResponse = () => {
-    if (res.status == 200){
-        let data = JSON.parse(this.responseText)
-        if ( data.access_token != undefined) {
-            access_token = data.access_token
-            localStorage.set('refresh_token', refresh_token)
-        }
-        if ( data.refresh_token != undefined) {
-            refresh_token = data.refresh_token
-            localStorage.setItem('refresh_token', refresh_token)
-        }
-        onPageLoad()
-    }
-}
-
-const getCode = () => {
-    let code = null
-    const queryString = window.location.search
-    if ( queryString.length > 0 ) {
-        const urlParams = new URLSearchParams(queryString)
-        code = urlParams.get('code')
-    }
-    return code
-}
 
 const requestAuthorization = () => {
     let authURL = 'https://accounts.spotify.com/authorize'
