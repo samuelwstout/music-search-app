18bc0cd85bb8e75ec7cf8233d7f980f02b3cd685
samuelwstout <samuelwstout@gmail.com>
2022-03-15 14:58:11 -0500

    Update styling and functionality from first project

--- index.js
+++ index.js
@@ -1,53 +1,64 @@
+// API Documentation: https://developer.apple.com/library/archive/documentation/AudioVideo/Conceptual/iTuneSearchAPI/index.html#//apple_ref/doc/uid/TP40017632-CH3-SW1
 
 //DOMContentLoaded for the submit form from index.html
 document.addEventListener('DOMContentLoaded', () => {
     const searchInput = document.querySelector('#textsearch')
+
     const handleInput = (e) => {
         var resetbutton = document.querySelector('#resetbutton')
         if (e) {
             resetbutton.style.backgroundColor = 'transparent'
         }
     }
+
     searchInput.oninput = handleInput
+
     resetbutton.addEventListener('click', (e) => {
         if (e) {
             resetbutton.style.backgroundColor = 'white'
         }
     })
     document.querySelector('#textsearch').addEventListener('input', (e) => {
-      if (e.target.value === '') {
-        resetbutton.style.backgroundColor = 'white'
-      }
-    })
+        if (e.target.value === '') {
+          resetbutton.style.backgroundColor = 'white'
+        }
+      })
+    
     document.querySelector('form').addEventListener('submit', (e) => {
         e.preventDefault()
-        searchAPI(e.target.textsearch.value) //I need 'textsearch' to display correct results?
+        searchAPI(e.target.textsearch.value)
     })
 })
-
-let clientID = 'a45eb12484d24c4199050bdefee6d24b'
-let clientSecret = 'f9765ecc897c46a8998cd3508002ae86'
-const redirectURI = 'http://127.0.0.1:5500/index.html'
-const scope = 'user-read-private user-read-email'
-const TOKEN = 'https://accounts.spotify.com/api/token'
-
-
-const requestAuthorization = () => {
-    let authURL = 'https://accounts.spotify.com/authorize'
-    authURL += '?client_id=' + clientID
-    authURL += '&response_type=code'
-    authURL += '&redirect_uri=' + encodeURI(redirectURI)
-    authURL += '&scope=' + scope
-    authURL += '&show_dialog=true'
-    window.location.href = authURL //show Spotify's authorization screen
-}
-
-
 //Add user input to the DOM
-const searchAPI = (search) => {
+function searchAPI (search) {
     let p = document.createElement('p')
     p.textContent = search
+
+//API URL 
+    const BASE_URL = "https://itunes.apple.com/search?"
+    let SEARCH_PATH = search
+    let FINAL_URL = `${BASE_URL}term=${SEARCH_PATH}&media=music&limit=20`
+
+//Fetch Request
+   fetch(FINAL_URL)
+    .then(res => {
+     return res.json()
+    })
+   .then(json => {
+        const html = json.results      
+        .map(searchTerm => {
+            return `
+            <div class="musician">
+                <p><img src=${searchTerm.artworkUrl100}></img></p>
+                <p><a class="resultText" target="_blank" rel="noopener noreferrer" href=${searchTerm.artistViewUrl}>${searchTerm.artistName}</a></p>
+                <p><a class="resultText" target="_blank" rel="noopener noreferrer" href=${searchTerm.trackViewUrl}>${searchTerm.trackName} | </a>
+                <a class="resultText" target="_blank" rel="noopener noreferrer" href=${searchTerm.collectionViewUrl}>${searchTerm.collectionName}</a></p>
+                <audio controls src=${searchTerm.previewUrl}> Your browser does not support the <code>audio</code> element.</audio>
+            </div>
+             `
+        })
+if ( ! (document.querySelector('#result-list').innerHTML = html.join(''))) {
+    document.querySelector('#result-list').innerHTML = `<h2 class='error'>Your search '${search}' does not match.</h2>`
 }
-
-
-  //https://accounts.spotify.com/authorize?response_type=token&client_id=a45eb12484d24c4199050bdefee6d24b&scope=user-read-private user-read-email&redirect_uri=http://localhost:8888
+})
+}
